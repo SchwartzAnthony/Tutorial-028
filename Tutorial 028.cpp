38dd58ffda676cad696b7759d927bb18a46d7b9e
SchwartzAJ <schwartzanthonyj@gmail.com>
2023-03-08 09:50:21 +0100

    Add project files.

---
+++ Tutorial 028.cpp
@@ -0,0 +1,16 @@
+// Tutorial 028.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "off.cpp"
+
+
+int main()
+{
+    std::cout << off::doSomething(5,4) << '\n';
+    std::cout << ogg::doSomething(5,4) << '\n'; 
+
+    return 0; 
+}
+
+
---
+++ ogg.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+namespace ogg
+{
+	int doSomething(int x, int y)
+	{
+		return x - y; 
+	}
+}
